3fe0474f525f794292986806482794a5545fd019
Vasily Gotovko <vasily.gotovko@gmail.com>
2013-01-08 09:51:23 +0300

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_open_version_filter do
+  name 'Redmine Open Version Filter plugin'
+  author '//Twinslash'
+  description "This plugin adds a new filter 'in open versions'"
+  version '0.0.1'
+  url 'https://github.com/twinslash/redmine_open_version_filter'
+  author_url 'http://twinslash.com'
+end
