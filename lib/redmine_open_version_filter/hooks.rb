d3eeebd072e5d8fdc520b098868a6617d3ca5e05
Vasily Gotovko <vasily.gotovko@gmail.com>
2013-01-15 16:03:19 +0300

    Fix filter in Queries page

--- lib/redmine_open_version_filter/hooks.rb
+++ lib/redmine_open_version_filter/hooks.rb
@@ -1,7 +1,8 @@
 module RedmineOpenVersionFilter
   class Hooks < Redmine::Hook::ViewListener
     def view_layouts_base_html_head(context={})
-      if context[:controller] && context[:controller].is_a?(IssuesController)
+      if context[:controller] &&
+        (context[:controller].is_a?(IssuesController) || context[:controller].is_a?(QueriesController))
         javascript_include_tag('open_version_filter.js', :plugin => 'redmine_open_version_filter')
       end
     end
