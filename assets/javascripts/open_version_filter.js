d06ad90f3bf7929d1cdb805f19ca888875d6f9c8
Vasily Gotovko <vasily.gotovko@gmail.com>
2013-01-28 11:54:05 +0300

    Revert wrong refactoring JS

--- assets/javascripts/open_version_filter.js
+++ assets/javascripts/open_version_filter.js
@@ -1,14 +1,28 @@
-buildFilterRowWithoutOpenVersion = buildFilterRow;
-
-function buildFilterRow(field, operator, values) {
-  if ((field == 'in_open_versions') || (field == 'out_of_opened_versions')) {
-    buildFilterRowWithOpenVersion(field, operator, values);
+// this is patch for js function from core
+function addFilter(field, operator, values) {
+  var fieldId = field.replace('.', '_');
+  var tr = $('#tr_'+fieldId);
+  if (tr.length > 0) {
+    tr.show();
   } else {
-    buildFilterRowWithoutOpenVersion(field, operator, values);
+    // ========= patch start ============
+    if ((field == 'in_open_versions') || (field == 'out_of_opened_versions')) {
+      buildFilterRowPatch(field, operator, values);
+    } else {
+      buildFilterRow(field, operator, values);
+    }
+    // ========= patch end ============
   }
+  $('#cb_'+fieldId).attr('checked', true);
+  toggleFilter(field);
+  $('#add_filter_select').val('').children('option').each(function(){
+    if ($(this).attr('value') == field) {
+      $(this).attr('disabled', true);
+    }
+  });
 }
 
-function buildFilterRowWithOpenVersion(field, operator, values) {
+function buildFilterRowPatch(field, operator, values) {
   var fieldId = field.replace('.', '_');
   var filterTable = $("#filters-table");
   var filterOptions = availableFilters[field];
