9bfbf7c795fb2f1d378e441e58a461d9fc6bffc4
Vasily Gotovko <vasily.gotovko@gmail.com>
2013-01-29 16:40:02 +0300

    Join two filters in one. Use checkbox with selectbox

--- assets/javascripts/open_version_filter.js
+++ assets/javascripts/open_version_filter.js
@@ -6,7 +6,7 @@ function addFilter(field, operator, values) {
     tr.show();
   } else {
     // ========= patch start ============
-    if ((field == 'in_open_versions') || (field == 'out_of_opened_versions')) {
+    if (field == 'from_versions_open_version_filter') {
       buildFilterRowPatch(field, operator, values);
     } else {
       buildFilterRow(field, operator, values);
@@ -26,14 +26,32 @@ function buildFilterRowPatch(field, operator, values) {
   var fieldId = field.replace('.', '_');
   var filterTable = $("#filters-table");
   var filterOptions = availableFilters[field];
+  var filterValues = filterOptions['values'];
+  var i, select;
 
   var tr = $('<tr class="filter">').attr('id', 'tr_'+fieldId).html(
-    '<td class="field"><input checked="checked" id="cb_'+fieldId+'" name="f[]" value="'+field+'" type="checkbox">' +
-    '<label for="cb_'+fieldId+'"> '+filterOptions['name']+'</label></td>' +
-    "<td class='operator'><input id='operators_#{field}' name=op['#{field}'] type=hidden ></td>" +
-    '<td class="operator"></td>' +
+    '<td class="field"><input checked="checked" id="cb_'+fieldId+'" name="f[]" value="'+field+'" type="checkbox"><label for="cb_'+fieldId+'"> '+filterOptions['name']+'</label></td>' +
     '<td class="values"></td>'
   );
-
   filterTable.append(tr);
+
+
+  tr.find('td.values').append(
+    '<span style="display:none;"><select class="value" id="values_'+fieldId+'_1" name="v['+field+'][]"></select>'
+  );
+
+  select = tr.find('td.values select');
+  for (i=0;i<filterValues.length;i++){
+    var filterValue = filterValues[i];
+    var option = $('<option>');
+    if ($.isArray(filterValue)) {
+      option.val(filterValue[1]).text(filterValue[0]);
+      if ($.inArray(filterValue[1], values) > -1) {option.attr('selected', true);}
+    } else {
+      option.val(filterValue).text(filterValue);
+      if ($.inArray(filterValue, values) > -1) {option.attr('selected', true);}
+    }
+    select.append(option);
+  }
+
 }
--- lib/redmine_open_version_filter/open_version_filter_query_patch.rb
+++ lib/redmine_open_version_filter/open_version_filter_query_patch.rb
@@ -6,7 +6,6 @@ module OpenVersionFilterQueryPatch
       unloadable
 
       alias_method_chain :available_filters, :open_version_filter
-      alias_method_chain :add_filter_error, :open_version_filter
     end
   end
 
@@ -16,40 +15,26 @@ module OpenVersionFilterQueryPatch
     def available_filters_with_open_version_filter
       filters = available_filters_without_open_version_filter
 
-      # filter: in_open_versions
-      filters.merge!('in_open_versions' =>
-        {:type => :in_open_versions,
-         :values => [],
-         :order => 16,
-         :name => l("field_in_open_versions")})
-
-      # filter: out_of_opened_versions
-      filters.merge!('out_of_opened_versions' =>
-        {:type => :out_of_opened_versions,
-         :values => [],
-         :order => 16,
-         :name => l("field_out_of_opened_versions")})
+      filters.merge!('from_versions_open_version_filter' =>
+        {
+          :name => l('field_in_opened_versions'),
+          :order => 1,
+          :values => [[l(:in_opened_versions), :in_opened_versions], [l(:out_of_opened_versions), :out_of_opened_versions]],
+        })
     end
 
-    # select issues in versions which are opened and with date
-    def sql_for_in_open_versions_field(field, operator, value)
+    def sql_for_from_versions_open_version_filter_field(field, operator, value)
       scope = project && project.shared_versions || Version
-      version_ids = scope.open.visible.all(:conditions => 'effective_date IS NOT NULL').collect(&:id).push(0)
-      # do not care about operator and value - just add a condition if filter "in_open_versions" is enabled
-      "(#{Issue.table_name}.fixed_version_id IN (#{version_ids.join(',')}))"
-    end
+      if value == ["in_opened_versions"]
+        version_ids = scope.open.visible.all(:conditions => 'effective_date IS NOT NULL').collect(&:id).push(0)
+        # do not care about operator and value - just add a condition if filter "in_open_versions" is enabled
+        "(#{Issue.table_name}.fixed_version_id IN (#{version_ids.join(',')}))"
+      elsif value == ['out_of_opened_versions']
+        version_ids = scope.open.visible.all(:conditions => 'effective_date IS NULL').collect(&:id).push(0)
+        # do not care about operator and value - just add a condition if filter "in_open_versions" is enabled
+        "(#{Issue.table_name}.fixed_version_id IN (#{version_ids.join(',')}) OR #{Issue.table_name}.fixed_version_id IS NULL)"
+      end
 
-    # select issues without versions or in versions which are opened and without date
-    def sql_for_out_of_opened_versions_field(field, operator, value)
-      scope = project && project.shared_versions || Version
-      version_ids = scope.open.visible.all(:conditions => 'effective_date IS NULL').collect(&:id).push(0)
-      # do not care about operator and value - just add a condition if filter "in_open_versions" is enabled
-      "(#{Issue.table_name}.fixed_version_id IN (#{version_ids.join(',')}) OR #{Issue.table_name}.fixed_version_id IS NULL)"
-    end
-
-    # suppress errors for plugin's filters
-    def add_filter_error_with_open_version_filter(field, message)
-      add_filter_error_without_open_version_filter(field, message) unless field == 'in_open_versions' || field == 'out_of_opened_versions'
     end
 
   end
