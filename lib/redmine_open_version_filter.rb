5f4739d2e4508100f8455695d0aed68098ee9659
Eugene Batogov <johnbat26@gmail.com>
2014-04-01 00:21:36 +0400

    Fix2 for new redmine versions

--- lib/redmine_open_version_filter.rb
+++ lib/redmine_open_version_filter.rb
@@ -1,4 +1,4 @@
 require 'redmine_open_version_filter/open_version_filter_query_patch'
 Rails.configuration.to_prepare do
-  IssueQuery.send(:include, IssueQueryPatch)
+  IssueQuery.send(:include, OpenVersionFilterQueryPatch)
 end
